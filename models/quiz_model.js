7d811b831ff51a64506e8d6b19e2343880885953
Alejandro Ravelo <alu0100763379@ull.edu.es>
2015-11-25 22:02:52 +0000

    Comprobada la clase Respuesta con el resto de preguntas

--- models/quiz_model.js
+++ models/quiz_model.js
@@ -9,54 +9,38 @@ function Quiz() {
       respuesta: new Respuesta(/^\s*roma\s*$/i)
     },
     { pregunta: '¿Qué animal tiene en su nombre las cinco vocales?',
-      respuesta: function(x) {
-        return (/^\s*murci[eé]lago\s*$/i).exec(x);
-      }
+      respuesta: new Respuesta(/^\s*murci[eé]lago\s*$/i)
     },
     { pregunta: 'Si me nombras desaparezco. ¿Qué soy?',
-      respuesta: function(x) {
-        return (/^\s*silencio\s*$/i).exec(x);
-      }
+      respuesta: new Respuesta(/^\s*silencio\s*$/i)
     },
     { pregunta: 'Si: 111 = 13, 112 = 24, 113 = 35, 114 = 46. Entonces 115 = ?',
-      respuesta: function(x) {
-        return (/^\s*57\s*$/i).exec(x);
-      }
+      respuesta: new Respuesta(/^\s*57\s*$/i)
     },
     { pregunta: 'No estoy vivo pero crezco. No tengo pulmones pero necesito el aire. No tengo boca pero el agua me mata. ¿Qué soy?',
-      respuesta: function(x) {
-        return (/^\s*fuego\s*$/i).exec(x);
-    }
+      respuesta: new Respuesta(/^\s*fuego\s*$/i)
     },
     { pregunta: '¿Cuántos años tiene un lustro?',
-      respuesta: function(x) {
-        return (/^\s*5\s*$/i).exec(x);
-    }
+      respuesta: new Respuesta(/^\s*5\s*$/i)
     },
     { pregunta: '¿Cómo se llama el lugar rodeado de tres cuerdas donde pelean los boxeadores?',
-      respuesta: function(x) {
-        return (/^\s*ring\s*$/i).exec(x);
-    }
+      respuesta: new Respuesta(/^\s*ring\s*$/i)
     },
     { pregunta: '¿Cuál es el color que representa la esperanza?',
-      respuesta: function(x) {
-        return (/^\s*verde\s*$/i).exec(x);
-    }
+      respuesta: new Respuesta(/^\s*verde\s*$/i)
     },
     { pregunta: '¿Qué cantidad de huesos tiene en total el ser humano?',
-      respuesta: function(x) {
-        return (/^\s*206\s*$/i).exec(x);
-    }
+      respuesta: new Respuesta(/^\s*206\s*$/i)
     },
     {
       pregunta: '¿Quién reinaba en España cuando se descubrió América?',
-      respuesta: function(x) {
+      respuesta: new Respuesta(function(x) {
         if ((/\b(Isabel\s+y?\s*Fernando)|(Fernando\s+[ey]?\s*Isabel)\b/i).exec(x)) {
           return true;
         }
         if ((/\breyes\s+cat[oó]licos\b/i).exec(x)) { return true; }
         return false;
-      },
+      }),
     }
   );
   // insertar unas cuantas preguntas sobre
@@ -70,11 +54,11 @@ function Quiz() {
       debug("n2 = "+n2);
       self.q.push(
         { pregunta: '¿ '+n1+'x'+n2+"= ?",
-          respuesta: function(x) {
+          respuesta: new Respuesta(function(x) {
             debug("n1 = "+n1);
             debug("n2 = "+n2);
             return (x == n1*n2);
-        }
+        })
       })
     })();
   }
