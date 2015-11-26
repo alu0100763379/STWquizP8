0ec1594c0c06b077b973c567049b6135c28f8a00
Alejandro Ravelo <alu0100763379@ull.edu.es>
2015-11-26 09:29:26 +0000

    Creada la condición para pregunta corta o pregunta larga en la clase Pregunta

--- models/pregunta_model.js
+++ models/pregunta_model.js
@@ -3,12 +3,21 @@ function Pregunta(prgt){
   this.view;
 }
 
-Question.prototype.get_prgt_type = function(){
+Pregunta.prototype.get_prgt_type = function(){
   return this.view;
 }
 
-Question.prototype.get_prgt = function(){
+Pregunta.prototype.get_prgt = function(){
   return this.prgt;
+}
+
+Pregunta.prototype.Type = function() {
+  if(this instanceof PreguntaCorta){
+    this.view = "<input type='text' name='respuesta' placeholder='Responda aquí' autofocus=''>";
+  }
+  else {
+    this.view = "<textarea type='text' name='respuesta' rows='5' cols='50' placeholder='Responda aquí' autofocus=''></textarea>";
+  }
 }
 
 module.exports = Pregunta;
