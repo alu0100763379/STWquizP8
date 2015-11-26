9d91847ec1232dc9d4364f8dbbcaa6874e3579ef
Alejandro Ravelo <alu0100763379@ull.edu.es>
2015-11-26 10:52:36 +0000

    Preguntas Funcionando (Error en las vistas)

--- models/quiz_model.js
+++ models/quiz_model.js
@@ -10,32 +10,32 @@ function Quiz() {
     { pregunta: new Corta('¿Capital de Italia?'),
       respuesta: new Respuesta(/^\s*roma\s*$/i)
     },
-    { pregunta: '¿Qué animal tiene en su nombre las cinco vocales?',
+    { pregunta: new Corta('¿Qué animal tiene en su nombre las cinco vocales?'),
       respuesta: new Respuesta(/^\s*murci[eé]lago\s*$/i)
     },
-    { pregunta: 'Si me nombras desaparezco. ¿Qué soy?',
+    { pregunta: new Corta('Si me nombras desaparezco. ¿Qué soy?'),
       respuesta: new Respuesta(/^\s*silencio\s*$/i)
     },
-    { pregunta: 'Si: 111 = 13, 112 = 24, 113 = 35, 114 = 46. Entonces 115 = ?',
+    { pregunta: new Larga('Si: 111 = 13, 112 = 24, 113 = 35, 114 = 46. Entonces 115 = ?'),
       respuesta: new Respuesta(/^\s*57\s*$/i)
     },
-    { pregunta: 'No estoy vivo pero crezco. No tengo pulmones pero necesito el aire. No tengo boca pero el agua me mata. ¿Qué soy?',
+    { pregunta: new Larga('No estoy vivo pero crezco. No tengo pulmones pero necesito el aire. No tengo boca pero el agua me mata. ¿Qué soy?'),
       respuesta: new Respuesta(/^\s*fuego\s*$/i)
     },
-    { pregunta: '¿Cuántos años tiene un lustro?',
+    { pregunta: new Corta('¿Cuántos años tiene un lustro?'),
       respuesta: new Respuesta(/^\s*5\s*$/i)
     },
-    { pregunta: '¿Cómo se llama el lugar rodeado de tres cuerdas donde pelean los boxeadores?',
+    { pregunta: new Larga('¿Cómo se llama el lugar rodeado de tres cuerdas donde pelean los boxeadores?'),
       respuesta: new Respuesta(/^\s*ring\s*$/i)
     },
-    { pregunta: '¿Cuál es el color que representa la esperanza?',
+    { pregunta: new Corta('¿Cuál es el color que representa la esperanza?'),
       respuesta: new Respuesta(/^\s*verde\s*$/i)
     },
-    { pregunta: '¿Qué cantidad de huesos tiene en total el ser humano?',
+    { pregunta: new Corta('¿Qué cantidad de huesos tiene en total el ser humano?'),
       respuesta: new Respuesta(/^\s*206\s*$/i)
     },
     {
-      pregunta: '¿Quién reinaba en España cuando se descubrió América?',
+      pregunta: new Corta('¿Quién reinaba en España cuando se descubrió América?'),
       respuesta: new Respuesta(function(x) {
         if ((/\b(Isabel\s+y?\s*Fernando)|(Fernando\s+[ey]?\s*Isabel)\b/i).exec(x)) {
           return true;
@@ -55,7 +55,7 @@ function Quiz() {
       var n2 = Math.randomInt(9)+1;
       debug("n2 = "+n2);
       self.q.push(
-        { pregunta: '¿ '+n1+'x'+n2+"= ?",
+        { pregunta: new Corta('¿ '+n1+'x'+n2+"= ?"),
           respuesta: new Respuesta(function(x) {
             debug("n1 = "+n1);
             debug("n2 = "+n2);
